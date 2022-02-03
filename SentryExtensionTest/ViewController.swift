172aefe39f02b89c6358d56a4fe3f1efd7efd426
Johann Behrens <jbehrens@transip.nl>
2022-02-03 20:06:07 +0100

    Initial Commit

---
+++ SentryExtensionTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SentryExtensionTest
+//
+//  Created by Johann Behrens on 03/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
